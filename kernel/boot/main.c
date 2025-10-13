65b885908e7e14d000608c645ac39a714b8ae144
lky <2948430900@qq.com>
2025-10-13 16:23:17 +0800

    lab2_phase2

--- kernel/boot/main.c
+++ kernel/boot/main.c
@@ -2,6 +2,7 @@
 #include "lib/print.h"
 #include "riscv.h"
 #include "mem/pmem.h"
+#include "mem/vmem.h"
 #include "lib/str.h"
 #include "common.h"
 
@@ -10,67 +11,48 @@ volatile static int started = 0;
 volatile static int over_1 = 0, over_2 = 0;
 
 // 保存分配的物理页指针
-static int* mem[1024];
+// static int* mem[1024];
 
-int main(void) {
-    int cpuid = r_tp(); // 获取当前 CPU id
+int main()
+{
+    int cpuid = r_tp();
 
     if(cpuid == 0) {
         uart_init();
         print_init();
         pmem_init();
+        kvm_init();
+        kvm_inithart();
 
         printf("cpu %d is booting!\n", cpuid);
-
         __sync_synchronize();
-        started = 1; // 唤醒其他核
+        // started = 1;
 
-        // ===== CPU0 分配 =====
-        int alloc_count_0 = 0;
-        for(int i = 0; i < 512; i++) {
-            mem[i] = pmem_alloc(true);
-            if(!mem[i]) break; // 分配失败直接停止
-            memset(mem[i], 1, PGSIZE);
-            printf("mem = %p, data = %d\n", mem[i], mem[i][0]);
-            alloc_count_0++;
-        }
-        printf("cpu %d alloc over\n", cpuid);
-        over_1 = 1;
+        pgtbl_t test_pgtbl = pmem_alloc(true);
+        uint64 mem[5];
+        for(int i = 0; i < 5; i++)
+            mem[i] = (uint64)pmem_alloc(false);
 
-        while(over_1 == 0 || over_2 == 0);
+        printf("\ntest-1\n\n");    
+        vm_mappages(test_pgtbl, 0, mem[0], PGSIZE, PTE_R);
+        vm_mappages(test_pgtbl, PGSIZE * 10, mem[1], PGSIZE / 2, PTE_R | PTE_W);
+        vm_mappages(test_pgtbl, PGSIZE * 512, mem[2], PGSIZE - 1, PTE_R | PTE_X);
+        vm_mappages(test_pgtbl, PGSIZE * 512 * 512, mem[2], PGSIZE, PTE_R | PTE_X);
+        vm_mappages(test_pgtbl, VA_MAX - PGSIZE, mem[4], PGSIZE, PTE_W);
+        vm_print(test_pgtbl);
 
-        // ===== CPU0 释放 =====
-        for(int i = 0; i < alloc_count_0; i++) {
-            pmem_free((uint64)mem[i], true);
-        }
-        printf("cpu %d free over\n", cpuid);
+        printf("\ntest-2\n\n");    
+        vm_mappages(test_pgtbl, 0, mem[0], PGSIZE, PTE_W);
+        vm_unmappages(test_pgtbl, PGSIZE * 10, PGSIZE, true);
+        vm_unmappages(test_pgtbl, PGSIZE * 512, PGSIZE, true);
+        vm_print(test_pgtbl);
 
     } else {
+
         while(started == 0);
         __sync_synchronize();
-
         printf("cpu %d is booting!\n", cpuid);
-
-        // ===== CPU1 分配 =====
-        int alloc_count_1 = 0;
-        for(int i = 512; i < 1024; i++) {
-            mem[i] = pmem_alloc(true);
-            if(!mem[i]) break;
-            memset(mem[i], 1, PGSIZE);
-            printf("mem = %p, data = %d\n", mem[i], mem[i][0]);
-            alloc_count_1++;
-        }
-        printf("cpu %d alloc over\n", cpuid);
-        over_2 = 1;
-
-        while(over_1 == 0 || over_2 == 0);
-
-        // ===== CPU1 释放 =====
-        for(int i = 512; i < 512 + alloc_count_1; i++) {
-            pmem_free((uint64)mem[i], true);
-        }
-        printf("cpu %d free over\n", cpuid);
+         
     }
-
-    while(1);
+    while (1);    
 }
